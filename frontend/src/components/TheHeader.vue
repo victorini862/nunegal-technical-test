580edbecfdff69038a49023b844f3e23689d1e43
Víctor González <victorgonzalez862@gmail.com>
2025-03-08 16:19:45 +0100

    [MOD] The header style is changed

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <header>
-    <h1 @click="$router.push('/')">Mobile Store</h1>
-    <span>🛒 {{ productStore.cartCount }}</span>
+  <header class="header">
+    <h1 class="logo" @click="$router.push('/')">Mobile Store</h1>
+    <span class="cart-icon">🛒 {{ productStore.cartCount }}</span>
   </header>
 </template>
 
@@ -12,12 +12,39 @@ const productStore = useProductStore();
 </script>
 
 <style>
-header {
+body {
+  font-family: 'Roboto', sans-serif; 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #f4f4f4; 
+}
+
+.header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 20px;
-  background: #333;
+  background: #397eda; 
   color: white;
   cursor: pointer;
+  box-shadow: 0 4px 6px rgba(46, 44, 199, 0.1); 
+}
+
+.logo {
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+.cart-icon {
+  font-size: 1.5rem;
+  background-color: #ff9800; 
+  padding: 10px;
+  border-radius: 50%;
+  transition: background-color 0.3s ease;
+}
+
+.cart-icon:hover {
+  background-color: #e65100; 
 }
 </style>
